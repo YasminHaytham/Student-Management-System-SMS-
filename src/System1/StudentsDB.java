1ea31b1ff5a7f1a20b529b21261cb11c4bc9ff35
FarahAli04 <es.farah.ali2023@alexu.edu.eg>
2025-10-25 17:48:39 +0300

    Backend1.1

---
+++ src/System1/StudentsDB.java
@@ -0,0 +1,118 @@
+package System1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+public class StudentsDB {
+
+    private String filename;
+    private ArrayList<Student> records;
+    
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public void setRecords(ArrayList<Student> records) {
+        this.records = records;
+    }
+
+    public StudentsDB(String filename) {
+        if (filename == null || filename.trim().isEmpty()) {
+
+        } else {
+            this.filename = filename;
+        }
+
+        this.records = new ArrayList<>();
+        this.readFromFile();
+    }
+
+    public void readFromFile() {
+        records.clear();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (!line.trim().isEmpty()) {
+                    Student record = createRecordFrom(line);
+                    if (record != null) {
+                        records.add(record);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading The file: " + e.getMessage());
+        }
+    }
+
+    public ArrayList<Student> returnAllRecords() {
+        return records;
+    }
+
+    public boolean contains(int Student_ID) {
+        for (Student record : records) {
+            if (record.getStudent_ID() == Student_ID) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Student createRecordFrom(String line) {
+        String[] parts = line.split(",");
+        if (parts.length == 6) {
+            try {
+                int Student_ID = Integer.parseInt(parts[0]);
+                String Full_Name = parts[1].trim();
+                int age = Integer.parseInt(parts[2]);
+                String Gender = parts[3].trim();
+                String Department = parts[4].trim();
+                float GPA = Float.parseFloat(parts[5]);
+                return new Student(Student_ID, Full_Name, age, Gender, Department, GPA);
+            } catch (NumberFormatException e) {
+                System.out.println("Error" + e.getMessage());
+            }
+        }
+
+        return null;
+    }
+    
+   public void insertRecord(Student record) {
+        if (contains(record.getStudent_ID())) {
+ 
+        } else {
+            records.add(record);
+        }
+    }
+
+    public void saveToFile() {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(filename))) {
+            for (Student record : records) {
+                writer.println(record.lineRepresentation());
+            }
+        } catch (IOException e) {
+            System.out.println( "Error saving to The file : " + e.getMessage());
+        }
+    }
+
+    public Student getRecord(int key) {
+        for (Student record : records) {
+            if (record.getStudent_ID()==key) {
+                return record;
+            }
+        }
+        return null;
+    }
+
+    public void deleteRecord(int key) {
+        records.removeIf(record -> record.getStudent_ID() == key);
+    }
+}

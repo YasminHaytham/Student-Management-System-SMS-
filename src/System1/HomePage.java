0b35fc03111856c0f3bfe8e45ae6b0cd0bd0f0f6
Yahya <es.yehia.ali2023@alexu.edu.eg>
2025-10-26 23:46:56 +0300

    update

--- src/System1/HomePage.java
+++ src/System1/HomePage.java
@@ -9,7 +9,11 @@ public class HomePage extends javax.swing.JPanel {
     }
 
     private void setupButtonListeners() {
+<<<<<<< Updated upstream
        Addbtn.addActionListener(evt -> navigateToPanel(new AddStudent()));
+=======
+      // Addbtn.addActionListener(evt -> navigateToPanel(new AddStudent()));
+>>>>>>> Stashed changes
        Viewbtn.addActionListener(evt -> navigateToPanel(new ViewPage()));
        Updatebtn.addActionListener(evt -> navigateToPanel(new SAUPanel()));
         Deletebtn.addActionListener(evt -> navigateToPanel(new DeletePanel()));
--- src/System1/SAUPanel.java
+++ src/System1/SAUPanel.java
@@ -91,6 +91,7 @@ public class SAUPanel extends javax.swing.JPanel {
             .addGroup(layout.createSequentialGroup()
                 .addGap(37, 37, 37)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+<<<<<<< Updated upstream
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel2)
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
@@ -99,6 +100,11 @@ public class SAUPanel extends javax.swing.JPanel {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(saveButton)
                         .addGap(33, 33, 33))))
+=======
+                    .addComponent(jLabel2)
+                    .addComponent(NameOrID, javax.swing.GroupLayout.PREFERRED_SIZE, 327, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+>>>>>>> Stashed changes
             .addGroup(layout.createSequentialGroup()
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 979, Short.MAX_VALUE)
@@ -114,8 +120,13 @@ public class SAUPanel extends javax.swing.JPanel {
                     .addComponent(NameOrID, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(saveButton))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+<<<<<<< Updated upstream
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 699, Short.MAX_VALUE)
                 .addContainerGap())
+=======
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(238, Short.MAX_VALUE))
+>>>>>>> Stashed changes
         );
     }// </editor-fold>//GEN-END:initComponents
 
